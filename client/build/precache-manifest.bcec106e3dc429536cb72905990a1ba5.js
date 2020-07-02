a9dc7e566fcd26afa67beeb27246990119497789
Khaja S <khajabandenawaz193@gmail.com>
2020-07-02 21:30:51 +0530

    added client build

---
+++ client/build/precache-manifest.bcec106e3dc429536cb72905990a1ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c72c8b9d91c1f413a49bebe5eb09436d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f5f30da5373c4197e29",
+    "url": "/static/js/2.52dcc15a.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/static/js/2.52dcc15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d670d8be7284f78eab",
+    "url": "/static/js/main.43a05c23.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
